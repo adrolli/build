76abb9ac5181faf446519999082a136b19d37e25
Alf Drollinger <40421928+adrolli@users.noreply.github.com>
2024-10-08 10:37:08 +0200

    Initial commit

---
+++ src/ItemPlugin.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Moox\Builder;
+
+use Filament\Contracts\Plugin;
+use Filament\Panel;
+use Filament\Support\Concerns\EvaluatesClosures;
+use Moox\Builder\Resources\ItemResource;
+
+class ItemPlugin implements Plugin
+{
+    use EvaluatesClosures;
+
+    public function getId(): string
+    {
+        return 'builder';
+    }
+
+    public function register(Panel $panel): void
+    {
+        $panel->resources([
+            ItemResource::class,
+        ]);
+    }
+
+    public function boot(Panel $panel): void
+    {
+        //
+    }
+
+    public static function make(): static
+    {
+        return app(static::class);
+    }
+}
---
+++ src/Resources/ItemResource/Pages/CreateItem.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Moox\Builder\Resources\ItemResource\Pages;
+
+use Filament\Resources\Pages\CreateRecord;
+use Moox\Builder\Resources\ItemResource;
+
+class CreateItem extends CreateRecord
+{
+    protected static string $resource = ItemResource::class;
+}
